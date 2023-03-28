a0916c04196d2f6d3d9c1377e6ba014e739aa127
liuyunhe <liuyunhe666@aliyun.com>
2023-03-28 16:49:57 +0800

    购物车功能

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './ShoppingCart.module.css'
 import { FiShoppingCart } from 'react-icons/fi'
+import { appContext } from '../AppState'
 
 interface Props {}
 
@@ -28,21 +29,26 @@ class ShoppingCart extends React.Component<Props, State> {
 
   render() {
     return (
-      <div className={styles.cartContainer}>
-        <button className={styles.button} onClick={this.handleClick}>
-          <FiShoppingCart />
-          <span>购物车 2（件）</span>
-        </button>
-        <div
-          className={styles.cartDropDown}
-          style={{ display: this.state.isOpen ? 'block' : 'none' }}
-        >
-          <ul>
-            <li>robot 1</li>
-            <li>robot 2</li>
-          </ul>
-        </div>
-      </div>
+      <appContext.Consumer>
+        {(value) => (
+          <div className={styles.cartContainer}>
+            <button className={styles.button} onClick={this.handleClick}>
+              <FiShoppingCart />
+              <span>购物车 {value.shoppingCart.items.length}（件）</span>
+            </button>
+            <div
+              className={styles.cartDropDown}
+              style={{ display: this.state.isOpen ? 'block' : 'none' }}
+            >
+              <ul>
+                {value.shoppingCart.items.map((i) => (
+                  <li key={i.id}>{i.name}</li>
+                ))}
+              </ul>
+            </div>
+          </div>
+        )}
+      </appContext.Consumer>
     )
   }
 }
